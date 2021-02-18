46326924cd8cfd8e13cb977509971fb4e824b252
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-18 18:41:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ if (navigator.geolocation)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
+
+      map.on();
     },
     function () {
       alert('Could not get yout position');
