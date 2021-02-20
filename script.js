ae85dde5d6e58dd3babdbe0c23b87382f9710218
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-20 11:47:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ if (navigator.geolocation)
               className: 'running-popup',
             })
           )
+          .setPopupContent('Workout')
           .openPopup();
       });
     },
